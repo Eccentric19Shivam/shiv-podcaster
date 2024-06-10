95b9e13a3eeda6ef10c33c863e0b31b249e22589
Eccentric19Shivam <singhshivamhp16@gmail.com>
2024-06-10 18:57:59 +0530

    clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+// https://touching-seahorse-35.clerk.accounts.dev
+const authConfig = {
+    providers: [
+      {
+        domain: "https://touching-seahorse-35.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig
